315da374937797bb7e102aad7a8fbe13577c1d96
Dhrumil Parikh <dhrumilparikh02@gmail.com>
2025-08-28 16:27:46 +0530

    feat : initial flutter project setup with clean architecture structure

---
+++ lib/app/export.dart
@@ -0,0 +1,2 @@
+export 'package:flutter/material.dart';
+export 'package:feed_app/app/app.dart';
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:feed_app/app/export.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const FeedApp());
+}
+
+
